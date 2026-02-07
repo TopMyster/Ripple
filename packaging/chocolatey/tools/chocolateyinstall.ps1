2704350363031c52f7edbdb1f3205cd583d9e4cb
TopMyster <topmyster@outlook.com>
2026-02-07 14:26:53 -0600

    Delete packaging directory

--- packaging/chocolatey/tools/chocolateyinstall.ps1
+++
@@ -1,9 +0,0 @@
-$packageName = 'ripple'
-$fileType = 'exe'
-$url = 'https://github.com/TopMyster/Ripple/releases/download/v2.3.3/Ripple-Setup-2.3.3.exe'
-$silentArgs = '/S'
-
-Install-ChocolateyPackage -PackageName $packageName `
-                          -FileType $fileType `
-                          -SilentArgs $silentArgs `
-                          -Url $url
